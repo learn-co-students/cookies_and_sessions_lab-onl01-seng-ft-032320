85b63482e1ab9a1f5b0ecdbf8afa8f988c006c39
mariamfh9 <mfh.haider@gmail.com>
2020-06-10 12:41:40 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,27 @@
+  
+class ProductsController < ApplicationController 
+
+    def index 
+        # binding.pry
+        # @cart = controller.cart
+        # @cart = session[:cart]
+    end
+
+    def add
+
+    end
+
+    def add_item
+        # binding.pry
+        if session[:cart]
+            session[:cart] << params[:product]
+        else 
+            session[:cart] = [params[:product]]
+        end
+        
+        redirect_to index_path
+    end
+
+
+
+end
